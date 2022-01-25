e6177aae65c10b3f1279f4b4a2034c631b32ac46
MPANO Christian <mpanoc6@gmail.com>
2022-01-25 19:41:26 +0200

    Revert "added passport middeware to handle login"
    This reverts commit cfd050dbe66e8f12ae5b147ce9653d2a84cf5dad.

--- config/database.js
+++ config/database.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 const dbConnect = async () => {
-  try {
-    await mongoose.connect('mongodb://localhost/passport-express', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('connected to db...');
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+    try {
+        await mongoose.connect('http://localhost/authDb', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    }catch(err) {
+        console.log(err.message);
+    }
+}
 module.exports = dbConnect;
