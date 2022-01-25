86267829387c804ac820226bd433dd948b3f6251
MPANO Christian <mpanoc6@gmail.com>
2022-01-25 13:32:26 +0200

    added routing

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const dbConnect = require('./config/database');
 const dotenv = require('dotenv');
 dotenv.config({
     path: './.env'
 })
+dbConnect();
 const app = express();
-
+app.use(express.json());
 app.listen(process.env.PORT || 5000, () => {
     console.log(`app listening on port 5000`);
 })
